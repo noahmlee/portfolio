65219757e7646599486fab33f6232fd1170d217d
Noah Lee <noahsaures@gmail.com>
2024-08-29 10:36:54 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll effect
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            document.querySelector(link.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
